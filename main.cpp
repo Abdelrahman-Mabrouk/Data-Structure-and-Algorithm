412db173adc67e7a9d7603bd659a72bbf70fb17d
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2024-04-22 12:41:18 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class student {
+    string id;
+    string name;
+    double gba;
+public:
+    student(string , string , double );
+
+};
+int main() {
+    return 0;
+}
