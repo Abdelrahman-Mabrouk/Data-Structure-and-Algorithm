76d86f5825941fbf6bcd0ffc165b928888bf13b0
Abdelrahman  Mabrouk <125595697+Abdelrahman-Mabrouk@users.noreply.github.com>
2024-04-22 14:06:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,8 +15,21 @@ public:
         if(name<x.name)return true;
         else return false;
     }
+void  insertion_sort(int  array[],int n) {
+    for (int i = 1, j; i < n; ++i) {
+        int t = array[i];
+        for (j = i; j > 0 && t < array[j - 1]; j--) array[j] = array[j - 1];
+        array[j] = t;
+    }
+}
 
 };
 int main() {
+   int  a[]={1,5,3,6,8,2,4};
+
+    insertion_sort(a,7);
+    for (int i = 0; i < 7; ++i) {
+        cout<<a[i]<<" ";
+    }
     return 0;
 }
